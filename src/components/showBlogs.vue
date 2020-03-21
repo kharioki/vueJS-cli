df14a329de5ed57d1cf86fcd06a019c6ff01a3c7
kharioki <antolisah@gmail.com>
2020-03-21 07:22:45 +0300

    retrieving posts from firebase

--- src/components/showBlogs.vue
+++ src/components/showBlogs.vue
@@ -4,7 +4,7 @@
       <input type="text" v-model="search" placeholder="search blogs" />
       <div v-for="blog in filteredBlogs" class="single-blog">
           <router-link v-bind:to="'/blog/' + blog.id"><h2>{{ blog.title | toUppercase }}</h2></router-link>
-          <article>{{ blog.body | snippet }}</article>
+          <article>{{ blog.content | snippet }}</article>
       </div>
   </div>
 </template>
@@ -23,9 +23,18 @@ export default {
   methods: {},
   created () {
     this.$http
-    .get('https://jsonplaceholder.typicode.com/posts')
+    .get('https://vue-ninja-playlist.firebaseio.com/posts.json')
     .then(data => {
-        this.blogs = data.body.slice(0, 10);
+      return data.json();
+    }).then(data => {
+      var blogsArray = [];
+
+      for(let key in data){
+        data[key].id = key;
+        blogsArray.push(data[key])
+      }
+
+      this.blogs = blogsArray
     });
   },
   computed: {},
@@ -37,9 +46,7 @@ export default {
   directives: {
       rainbow: {
         bind(el, binding, vnode) {
-          // el.style.color = '#' + Math.random().toString().slice(2, 8);
           el.style.color = '#' + Math.floor(Math.random() * 16777215).toString(16);
-          el.style.textDecoration = none
         }
       }
   },
--- src/components/singleBlog.vue
+++ src/components/singleBlog.vue
@@ -1,7 +1,11 @@
 <template>
     <div id="single-blog">
         <h1>{{ blog.title }}</h1>
-        <article>{{ blog.body }}</article>
+        <article>{{ blog.content }}</article>
+        <p>{{ blog.author }}</p>
+        <ul>
+            <li v-for="category in blog.categories">{{ category }}</li>
+        </ul>
     </div>
 </template>
 
@@ -15,9 +19,12 @@ export default {
     },
     created () {
         this.$http
-            .get('https://jsonplaceholder.typicode.com/posts/' + this.id)
+            .get('https://vue-ninja-playlist.firebaseio.com/posts/' + this.id + '.json')
             .then(data => {
-                this.blog = data.body
+                return data.json();
+            })
+            .then(data => {
+                this.blog = data
             });
     },
 }
